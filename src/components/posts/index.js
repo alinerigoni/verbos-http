7ec8d98d76dfccb7be5db1544363cde17202176e
Aline Rigoni <alinemfrontend@gmail.com>
2021-05-25 20:53:59 -0300

    Adicionado Bootstrap

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -1,21 +1,28 @@
 import PropTypes from 'prop-types';
+import { Row, Col } from 'react-bootstrap';
 import "./style.scss";
 
 const Posts = ({ posts }) => {
     console.log('posts', posts);
 
     return (
+
         <div className="posts">
-            {
-                posts.map((post, key) => {
-                    return (
-                        <div className="post" key={key}>
-                            <p className="title">{post.title}</p>
-                            <p className="text">{post.body}</p>
-                        </div>
-                    )
-                })
-            }
+            <Row>
+                {
+                    posts.map((post, key) => {
+                        return (
+                            <Col xs={4} key={key}>
+                                <div className="post" >
+                                    <p className="title">{post.title}</p>
+                                    <p className="text">{post.body}</p>
+                                </div>
+                            </Col>
+                        )
+                    })
+                }
+            </Row>
+
         </div>
     );
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
+import { Container } from 'react-bootstrap';
+
 // components of the page
 import Posts from '../../components/posts';
 
@@ -22,24 +24,30 @@ const Home = () => {
 
     return (
         <div className="pg-home">
-            <h1 className="pg-home__title">Verbos HTTP</h1>
-            <ul className="pg-home__list">
-                <li className="pg-home__list-item">Seguinte usando a lib axios, eu quero vc faça 3 telas pra testar os verbos http</li>
-                <li className="pg-home__list-item">1 Lista os dados</li>
-                <li className="pg-home__list-item">2 adicionar o dados</li>
-                <li className="pg-home__list-item">3 editar dados</li>
-                <li className="pg-home__list-item">Na listagem tem que ter um botão de deletar</li>
-                <li className="pg-home__list-item">Ao pra fazer as chamadas vc pode usar esse servir/api aqui</li>
-                <li className="pg-home__list-item"><a href="https://jsonplaceholder.typicode.com">https://jsonplaceholder.typicode.com</a></li>
-                <li className="pg-home__list-item">Sacou? Quero que vc faça um CRUD</li>
-                <li className="pg-home__list-item">Create, Read, Update, Delete</li>
-                <li className="pg-home__list-item">Os campos pode ser qualquer um tá bom?</li>
-                <li className="pg-home__list-item">Mas tem que ser mais no mínimo 3</li>
-                <li className="pg-home__list-item">A ideia é vc testar os verbos</li>
-                <li className="pg-home__list-item">E fazer crud</li>
-            </ul>
+            <div className="none">
+                <h1 className="pg-home__title">Verbos HTTP</h1>
 
-            <Posts posts={posts} />
+                <ul className="pg-home__list">
+                    <li className="pg-home__list-item">Seguinte usando a lib axios, eu quero vc faça 3 telas pra testar os verbos http</li>
+                    <li className="pg-home__list-item">1 Lista os dados</li>
+                    <li className="pg-home__list-item">2 adicionar o dados</li>
+                    <li className="pg-home__list-item">3 editar dados</li>
+                    <li className="pg-home__list-item">Na listagem tem que ter um botão de deletar</li>
+                    <li className="pg-home__list-item">Ao pra fazer as chamadas vc pode usar esse servir/api aqui</li>
+                    <li className="pg-home__list-item"><a href="https://jsonplaceholder.typicode.com">https://jsonplaceholder.typicode.com</a></li>
+                    <li className="pg-home__list-item">Sacou? Quero que vc faça um CRUD</li>
+                    <li className="pg-home__list-item">Create, Read, Update, Delete</li>
+                    <li className="pg-home__list-item">Os campos pode ser qualquer um tá bom?</li>
+                    <li className="pg-home__list-item">Mas tem que ser mais no mínimo 3</li>
+                    <li className="pg-home__list-item">A ideia é vc testar os verbos</li>
+                    <li className="pg-home__list-item">E fazer crud</li>
+                </ul>
+            </div>
+
+
+            <Container>
+                <Posts posts={posts} />
+            </Container>
         </div>
     )
 }
